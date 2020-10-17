b369d7d20f20b388e0f3e476ea5dac93c998c8e4
Prasad Jayakumar <prasad.jayakumar@gmail.com>
2020-10-17 23:30:29 +0530

    removed redundant computed properties

--- vue-2.6.12-coverage/app.js
+++ vue-2.6.12-coverage/app.js
@@ -2,8 +2,8 @@
 Vue.config.performance = true;
 
 // Define a new global component called button-counter
-Vue.component("button-counter", {
-  name: "button-counter",
+Vue.component('button-counter', {
+  name: 'button-counter',
   props: {
     initialCounter: Number,
     incrementValue: {
@@ -15,7 +15,7 @@ Vue.component("button-counter", {
     },
   },
   data() {
-    return {      
+    return {
       count: this.initialCounter,
     };
   },
@@ -23,67 +23,62 @@ Vue.component("button-counter", {
     '<button v-on:click="count=count+incrementValue">You clicked me {{ count }} times.</button>',
   // Lifecycle Hooks
   beforeCreate() {
-    this.$emit("event-msg", `beforeCreate`);
+    this.$emit('event-msg', 'beforeCreate');
   },
   created() {
-    this.$emit("event-msg", `created`);
+    this.$emit('event-msg', 'created');
   },
   beforeMount() {
-    this.$emit("event-msg", "beforeMount");
+    this.$emit('event-msg', 'beforeMount');
   },
   mounted() {
-    this.$emit("event-msg", "mounted");
+    this.$emit('event-msg', 'mounted');
   },
   beforeUpdate() {
-    this.$emit("event-msg", "beforeUpdate");
+    this.$emit('event-msg', 'beforeUpdate');
   },
   updated() {
-    this.$emit("event-msg", "updated");
+    this.$emit('event-msg', 'updated');
   },
   activated() {
-    this.$emit("event-msg", "activated");
+    this.$emit('event-msg', 'activated');
   },
   deactivated() {
-    this.$emit("event-msg", "deactivated");
+    this.$emit('event-msg', 'deactivated');
   },
   beforeDestroy() {
-    this.$emit("event-msg", "beforeDestroy");
+    this.$emit('event-msg', 'beforeDestroy');
   },
   destroyed() {
-    this.$emit("event-msg", "destroyed");
+    this.$emit('event-msg', 'destroyed');
   },
 });
 
 new Vue({
-  name: "demo",
-  el: "#demo",
+  name: 'demo',
+  el: '#demo',
 
   data: {
     bookUrl: '',
     rawHtml:
       "<span style='color:red'>Don't use v-html on user provided data</span>",
-    message: "Hello Vue",
-    firstName: "Prasad",
-    lastName: "Jayakumar",
+    firstName: 'Prasad',
+    lastName: 'Jayakumar',
 
     eventMsgs: [],
 
     counter: 5,
-    buttonCounter: "show",
+    buttonCounter: 'show',
   },
   computed: {
-    reversedMessage() {
-      // `this` points to the vm instance
-      return this.message.split("").reverse().join("");
-    },
     fullName: {
       // getter
       get() {
-        return this.firstName + " " + this.lastName;
+        return this.firstName + ' ' + this.lastName;
       },
       // setter
       set(newValue) {
-        var names = newValue.split(" ");
+        var names = newValue.split(' ');
         this.firstName = names[0];
         this.lastName = names[names.length - 1];
       },
@@ -91,7 +86,7 @@ new Vue({
   },
   methods: {
     suggestABook() {
-      this.bookUrl = "https://recommendmeabook.com/";
+      this.bookUrl = 'https://recommendmeabook.com/';
     },
     onEventMsg(id, msg) {
       this.eventMsgs.push({ id, msg });
