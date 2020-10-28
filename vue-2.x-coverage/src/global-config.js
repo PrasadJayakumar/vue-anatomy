03be7633204ff44cad88510e9e71be1efc80a8c8
Prasad Jayakumar <prasad.jayakumar@gmail.com>
2020-10-28 09:37:58 +0530

    performance measure included

--- vue-2.x-coverage/src/global-config.js
+++ vue-2.x-coverage/src/global-config.js
@@ -5,8 +5,9 @@ Vue.filter('titleCase', function (value) {
   return values.map((v) => v.charAt(0).toUpperCase() + v.slice(1)).join(' ');
 });
 
-Vue.config.productionTip = true;
-Vue.config.devtools = true;
+Vue.config.performance = true;
+// Vue.config.productionTip = true;
+// Vue.config.devtools = true;
 
 Vue.config.warnHandler = function (msg, vm, trace) {
   console.log(`Warn: ${msg}`);
